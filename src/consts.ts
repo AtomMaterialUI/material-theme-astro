1644fee73e13574c47a7919bd07f38db38889787
Elior <heliosaian@gmail.com>
2025-07-15 20:57:45 +0300

    Refactor `Head` component and extract metadata logic to new `Meta` component. Enhance project metadata and adjust Prettier config.

--- src/consts.ts
+++ src/consts.ts
@@ -2,8 +2,19 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Material Theme UI Documentation';
-export const SITE_DESCRIPTION = 'The Material Theme plugin is a plugin for JetBrains IDEs (IntelliJ, PhpStorm, WebStorm, Android Studio, etc...) which completely redesigns\n' +
+export const SITE_DESCRIPTION =
+  'The Material Theme plugin is a plugin for JetBrains IDEs (IntelliJ, PhpStorm, WebStorm, Android Studio, etc...) which completely redesigns\n' +
   '  the UI with a beautiful and configurable Material Design UI.\n' +
-  '  This is the plugin\'s documentation page which presents and explains the different features provided by the plugin.';
+  "  This is the plugin's documentation page which presents and explains the different features provided by the plugin.";
 export const SITE_URL = 'https://www.material-theme.com';
 export const SITE_AUTHOR = 'Elior Boukhobza (Mallowigi)';
+export const SITE_KEYWORDS = 'material,theme,plugin,material design,jetbrains,material theme,jetbrains plugin,oceanic,palenight,mallowigi';
+
+export const TWITTER_HANDLE = '@mallowigi';
+export const LOGO_PATH = '/img/brand/logo.png';
+
+export const VERSION = '9.2.0';
+export const YEAR = `2015-${new Date().getFullYear()}`;
+
+export const GITHUB_REPO = 'https://github.com/mallowigi/material-theme-issues';
+export const PLUGIN = 'https://plugins.jetbrains.com/plugin/8006-material-theme-ui';
