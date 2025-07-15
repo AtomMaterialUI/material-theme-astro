498436de6c09bc2499dcf7d8da1b0408b6b95906
Elior <heliosaian@gmail.com>
2025-07-15 20:47:47 +0300

    Refactor head components and optimize layouts for consistency and maintainability. Integrate Prettier with Astro plugin and adjust project metadata.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Material Theme UI Documentation';
+export const SITE_DESCRIPTION = 'The Material Theme plugin is a plugin for JetBrains IDEs (IntelliJ, PhpStorm, WebStorm, Android Studio, etc...) which completely redesigns\n' +
+  '  the UI with a beautiful and configurable Material Design UI.\n' +
+  '  This is the plugin\'s documentation page which presents and explains the different features provided by the plugin.';
+export const SITE_URL = 'https://www.material-theme.com';
+export const SITE_AUTHOR = 'Elior Boukhobza (Mallowigi)';
